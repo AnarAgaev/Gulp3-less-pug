084f08e001af5d38c526cbb54f4d9b491a85264e
Anar <a.agaev@smileexpo.ru>
2019-02-28 12:21:17 +0300

    Rename copy tasks to copy:name_of_the_file

--- gulpfile.js
+++ gulpfile.js
@@ -42,14 +42,13 @@ gulp.task('less', function () {
         .pipe(browserSync.stream())
 })
 
-gulp.task('resetcss', function () {
+gulp.task('copy:resetcss', function () {
     return gulp.src(['src/less/reset.css'])
         .pipe(sourcemaps.init())
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
-        .pipe(cleanCss())
         .pipe(sourcemaps.write('../maps'))
         .pipe(gulp.dest('web/style'))
         .pipe(browserSync.stream())
@@ -73,30 +72,30 @@ gulp.task('pug', function() {
     .pipe(browserSync.stream())
 })
 
-gulp.task('js', function () {
+gulp.task('copy:js', function () {
     return gulp.src('src/js/**/*.js')
         .pipe(gulp.dest('web/js'))
         .pipe(browserSync.stream())
 })
 
-gulp.task('imagemin', function () {
+gulp.task('copy:img', function () {
     return gulp.src('src/img/**/*')
         .pipe(imagemin())
         .pipe(gulp.dest('web/images'))
         .pipe(browserSync.stream())
 })
 
-gulp.task('serve', ['less', 'resetcss', 'pug', 'js', 'imagemin'], function () {
+gulp.task('serve', ['less', 'copy:resetcss', 'pug', 'copy:js', 'copy:img'], function () {
     browserSync.init({
         server: {
             baseDir: './web'
         }
     })
     gulp.watch('src/less/**/*.less', ['less'])
-    gulp.watch('src/less/reset.css', ['resetcss'])
+    gulp.watch('src/less/reset.css', ['copy:resetcss'])
     gulp.watch('src/pug/pages/**/*.pug', ['pug'])
-    gulp.watch('src/js/**/*.js', ['js'])
-    gulp.watch('src/img/**/*', ['imagemin'])
+    gulp.watch('src/js/**/*.js', ['copy:js'])
+    gulp.watch('src/img/**/*', ['copy:img'])
 })
 
 gulp.task('default', ['serve'])
